8429f178f1ad39fe81c167254d4ae05beac38421
Mac <mac@Macs-iMac.local>
2017-07-20 18:52:53 +0530

    Initial Commit

---
+++ Day 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day 2
+//
+//  Created by Mac on 20/07/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Day 2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Day 2
+//
+//  Created by Mac on 20/07/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
